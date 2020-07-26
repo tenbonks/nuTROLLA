b46a2371796105de21b9b1a023312b3c29de303b
Ben Tonks <thebentonks@gmail.com>
2020-07-26 14:03:05 +0100

    Added home app and templates

--- nutrolla/settings.py
+++ nutrolla/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',  # this model is present in admin panel
     'allauth.socialaccount',  # this model is present in admin panel
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,12 +59,15 @@ ROOT_URLCONF = 'nutrolla.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
