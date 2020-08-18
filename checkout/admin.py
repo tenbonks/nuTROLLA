e72c5a7ec6e39ab2e6dedd81a37fc332e8b3fedb
Ben Tonks <thebentonks@gmail.com>
2020-08-18 15:39:06 +0100

    Added signals, and configured checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+# Allows to edit lineitems from within the order in django admin
+class OrderLineItemAdminInLine(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInLine,)
+
+    readonly_fields = ('order_number', 'date', 'grand_total')
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+# not registering OrderLineItem as it can be accessed ti order model
+admin.site.register(Order, OrderAdmin)
