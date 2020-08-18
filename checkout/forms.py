fe0850123ccd23cdf77b6c709f2a1f42b4aefc4d
Ben Tonks <thebentonks@gmail.com>
2020-08-18 17:07:41 +0100

    Added checkout forms

---
+++ checkout/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
+
+
+def __init__(self, *args, **kwargs):
+    """
+    Add placeholder and classes, remove auto-generated labels,
+    and set autofocus on the first field of the form
+    """
+
+    super().__init__(*args, **kwargs)
+    placeholders = {
+        'full_name': 'Full Name',
+        'email': 'Email Address',
+        'phone_number': 'Phone Number',
+        'country': 'Country',
+        'postcode': 'Post Code',
+        'town_or_city': 'Town or City',
+        'street_address1': 'Street Address 1',
+        'street_address2': 'Street Address 2',
+        'county': 'County',
+    }
+
+    # autofocus on the full_name field upon page load
+    self.fields['full_name'].widget.attrs['autofocus'] = True
+
+    # itterate through the fields, add '*' to placeholder if it is required,
+    # set all placeholder attrs to their values from placeholders dict,
+    # add css class
+    for field in self.fields:
+        if self.field[field].required:
+            placeholder = f'{placeholders[field]} *'
+        else:
+            placeholder = placeholders[field]
+        self.fields[field].widget.attrs['palaceholder'] = placeholder
+        self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+        self.fields[field].label = False
