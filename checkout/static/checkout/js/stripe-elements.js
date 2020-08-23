f587ce8c5301340faf6818b892166ba2cc366dd1
Ben Tonks <thebentonks@gmail.com>
2020-08-23 20:21:52 +0100

    Passing checkout form values to stripe

--- checkout/static/checkout/js/stripe-elements.js
+++ checkout/static/checkout/js/stripe-elements.js
@@ -67,8 +67,36 @@ form.addEventListener('submit', function (ev) {
   $('#loading-overlay').fadeToggle(100);
   stripe.confirmCardPayment(clientSecret, {
     payment_method: {
-      card: card
+      card: card,
+      billing_details: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        email: $.trim(form.email.value),
+        address: {
+          line1: $.trim(form.street_address1.value),
+          line2:$.trim(form.street_address2.value),
+          city: $.trim(form.town_or_city.value),
+          country:$.trim(form.country.value),
+          state: $.trim(form.county.value),
+        }
+      }
+    },
+    shipping: {
+      card: card,
+      billing_details: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        email: $.trim(form.email.value),
+        address: {
+          line1: $.trim(form.street_address1.value),
+          line2:$.trim(form.street_address2.value),
+          city: $.trim(form.town_or_city.value),
+          postal_code:$.trim(form.country.value),
+          state: $.trim(form.county.value),
+        }
+      }
     }
+    
   }).then(function (result) {
     if (result.error) {
       var errorDiv = document.getElementById('card-errors');
