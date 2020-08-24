1b3a47471fe493806ff2633edceaac84dcdec7e1
Ben Tonks <thebentonks@gmail.com>
2020-08-24 02:12:26 +0100

    Added 2 more fields in Order model, added to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,12 +11,14 @@ class OrderLineItemAdminInLine(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInLine,)
 
-    readonly_fields = ('order_number', 'date', 'grand_total')
+    readonly_fields = ('order_number', 'date', 'grand_total',
+                       'original_bag', 'stripe_pid',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'grand_total',)
+              'street_address2', 'county', 'grand_total',
+              'original_bag', 'stripe_pid',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'grand_total',)
