9e9f0f39d088f160a6fe0064aa5f9fb13837faa0
Ben Tonks <thebentonks@gmail.com>
2020-08-25 14:42:15 +0100

    Completed the context processor, if no stock, remove id from bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from django.contrib import messages
+from .views import remove_from_bag
 
 
 def bag_contents(request):
@@ -9,8 +11,11 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
+    items_to_remove = []
+
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
+
         if quantity < product.stock:
             total += quantity * product.price
             product_count += quantity
@@ -20,19 +25,26 @@ def bag_contents(request):
                 'quantity': quantity,
                 'product': product,
             })
-        elif product.stock == 0:
-            if item_id in bag_items:
-                bag.pop()
+
         else:
             total += product.stock * product.price
             product_count += product.stock
 
-            bag_items.append({
-                'item_id': item_id,
-                'quantity': product.stock,
-                'product': product,
-            })
+            if product.stock != 0:
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': product.stock,
+                    'product': product,
+                })
+            else:
+                messages.error(request,
+                               f'{product.name} is now out of stock')
+                items_to_remove.append(item_id)
 
+    for item_id in items_to_remove:
+        if item_id:
+            print(item_id)
+            remove_from_bag(request, item_id)
     grand_total = total
 
     context = {
