2933b6989d19fddbf5d7ee1c1be00b33db1e55cb
Ben Tonks <thebentonks@gmail.com>
2020-08-26 17:05:03 +0100

    Made town_or_city required in the order model

--- checkout/models.py
+++ checkout/models.py
@@ -20,7 +20,7 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = CountryField(blank_label="Country *", null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
