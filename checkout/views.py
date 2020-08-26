4a34db03cf52225538ffab99942b302b274e6644
Ben Tonks <thebentonks@gmail.com>
2020-08-26 17:42:53 +0100

    Added prefill checkout form with default values if user is authenticated

--- checkout/views.py
+++ checkout/views.py
@@ -116,7 +116,24 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'county': profile.default_county,
+                    'postcode': profile.default_postcode,
+                    'country': profile.default_country,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning('Stripe public key is missing')
