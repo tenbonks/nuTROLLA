b2b1de26e7d2c3563828c9cfdfb11efb82834e95
Ben Tonks <thebentonks@gmail.com>
2020-08-27 20:07:41 +0100

    Added delete product view and url, redirecting to product detail after adding prod

--- products/views.py
+++ products/views.py
@@ -86,9 +86,9 @@ def add_product(request):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. \
                 Please ensure the form is valid.')
@@ -104,6 +104,7 @@ def add_product(request):
 
 
 def edit_product(request, product_id):
+    """ Edit a product in the store """
     product = get_object_or_404(Product, pk=product_id)
 
     if request.method == "POST":
@@ -125,3 +126,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
