30a0efc97fd0048159013ce7bd856a8345376521
Ben Tonks <thebentonks@gmail.com>
2020-08-27 21:57:12 +0100

    Console select in add product now uses its friendly name, also default choice is null

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Category, Console
+
+from collections import deque
 
 
 class ProductForm(forms.ModelForm):
@@ -11,9 +13,20 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        cat_friendly_names = ([(c.id, c.get_friendly_name())
+                              for c in categories])
+
+        consoles = Console.objects.all()
+        con_friendly_names = [(c.id, c.get_friendly_name()) for c in consoles]
+
+        add_null = (None, '------')
+
+        con_ammended_names = deque(con_friendly_names)
+        con_ammended_names.appendleft(add_null)
 
         # change the category input to use friendly names from above
-        self.fields['category'].choices = friendly_names
+        self.fields['category'].choices = cat_friendly_names
+
+        self.fields['console'].choices = con_ammended_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-gray rounded'
