a83081f9fb0228138741da02a82235375eac0222
Ben Tonks <thebentonks@gmail.com>
2020-08-28 19:32:26 +0100

    Removed secret key, and set debug to change if in development

--- nutrolla/settings.py
+++ nutrolla/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-#g+&lu&tz+!xa4=(-1@61p*n-8xobb&30wud=*^eez9647swg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'tenbonks-nutrolla.herokuapp.com']
 
