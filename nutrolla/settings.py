05575b9ec5c6daf4bf393b5d1779f6b75efb72bf
Ben Tonks <thebentonks@gmail.com>
2020-08-29 04:15:03 +0100

    Changed email backend if in out of development env to send emails via smtp

--- nutrolla/settings.py
+++ nutrolla/settings.py
@@ -104,8 +104,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # Allauth config
 ACCOUNT_AUTENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -205,6 +203,20 @@ if 'USE_AWS' in os.environ:
     # Overide static and media URLs in prooduction
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
+# Set email backend depending on if in development
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = "contact@nuTROLLA.com"
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_USER = os.environ.get('EMAIL_HOST_USER')
 
 # Stripe
 STRIPE_CURRENCY = 'gbp'
